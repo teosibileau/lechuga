a724c4d45599a5c7ca8b2ae512564786bae61d00
Teofilo Sibileau <teo.sibileau@gmail.com>
2018-11-30 17:55:56 -0300

    fix manually

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from distutils.core import setup
 from setuptools import find_packages
 
 required = [
-    '-i https://pypi.org/simple',
     'certifi==2018.11.29',
     'chardet==3.0.4',
     'click==7.0',
@@ -13,8 +12,7 @@ required = [
     'requests-cache==0.4.13',
     'requests==2.20.1',
     'simplejson==3.16.0',
-    'tabulate==0.8.2',
-    'urllib3==1.24.1; python_version < \'4\'',
+    'tabulate==0.8.2'
 ]
 
 setup(
